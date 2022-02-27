010cd0507da09d5cd6aee36d627437fe0a52ab4b
June <jonasjzy@gmail.com>
2022-02-27 20:40:38 +0800

    :fire:

--- example/lib/colorss.dart
+++ example/lib/colorss.dart
@@ -17,7 +17,7 @@ Container centerText(String text, {Color? color, VoidCallback? onPressed}) {
     height: 100,
     padding: EdgeInsets.all(30),
     color: color == null ? randomColor() : color,
-    child: Jelly(child: onPressed == null ? Text(text) : ElevatedButton(onPressed: onPressed, child: Text(text))),
+    child: onPressed == null ? Text(text) : ElevatedButton(onPressed: onPressed, child: Text(text)),
   );
 }
 
@@ -27,9 +27,7 @@ Container centerTextButton(String text, {Color? color, VoidCallback? onPressed})
     height: 100,
     padding: const EdgeInsets.all(30),
     color: color == null ? randomColor() : color,
-    child: Jelly(
-      child: onPressed == null ? Text(text) : TextButton(onPressed: onPressed, child: Text(text)),
-    ),
+    child: onPressed == null ? Text(text) : TextButton(onPressed: onPressed, child: Text(text)),
   );
 }
 
--- lib/card_flip.dart
+++ lib/card_flip.dart
@@ -1,3 +1,2 @@
 export 'src/flip_layout.dart';
-export 'src/jelly_layout.dart';
 export 'src/folding_box.dart';
